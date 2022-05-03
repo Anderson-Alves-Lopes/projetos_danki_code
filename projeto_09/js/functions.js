ae0c4a62e10fa1a22a5f73e92d03a2e419c42e23
ANDERSON ALVES LOPES <neno-alveslopes@hotmail.com>
2022-05-02 21:34:33 -0300

    finalizado

--- projeto_09/js/functions.js
+++ projeto_09/js/functions.js
@@ -1,8 +1,10 @@
 $(function(){
-    $('nav a').hover(function(){
-        $('nav li').removeClass('selected');
+    $('.desktop a').hover(function(){
+        $('.desktop li').removeClass('selected');
         $(this).parent().addClass('selected');
     })
-    
-    
+    $('nav.mobile h3').click(function(){
+        $('nav.mobile ul').slideToggle(1000);
+    })
+   
 });
